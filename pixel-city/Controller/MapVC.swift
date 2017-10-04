49d388e1af100f00944c31645ad6d5e2bdab9bb8
Nikka-Sher <npiterman@gmail.com>
2017-10-03 22:58:10 -0900

    does not auto-spawn pin at user location, only double click. tint changed to match header

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -52,6 +52,19 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
 }
 
 extension MapVC: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let pinAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "droppablePin")
+        pinAnnotation.pinTintColor = #colorLiteral(red: 0.9370436072, green: 0.6403255514, blue: 0.8714432711, alpha: 1)
+        pinAnnotation.animatesDrop = true
+        return pinAnnotation
+    }
+    
+    
     func centerMapOnUserLocation() {
         guard let coordinate = locationManager.location?.coordinate else { return }
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
